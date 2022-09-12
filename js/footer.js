66252f4f684534d2f1f37f5bd504c78cadf05bed
youganshmalik <113317494+youganshmalik@users.noreply.github.com>
2022-09-12 11:09:34 +0530

    Add files via upload

---
+++ js/footer.js
@@ -0,0 +1,69 @@
+const createFooter = () => {
+    let footer = document.querySelector('footer');
+
+    footer.innerHTML = `
+    <div class="footer-content">
+        <img src="img/image_2022-09-11_02-16-56.png" class="logo" alt="">
+        <div class="footer-ul-container">
+            <ul class="category">
+                <li class="category-title">men</li>
+                <li><a href="#" class="footer-link">t-shirts</a></li>
+                <li><a href="#" class="footer-link">sweatshirts</a></li>
+                <li><a href="#" class="footer-link">shirts</a></li>
+                <li><a href="#" class="footer-link">jeans</a></li>
+                <li><a href="#" class="footer-link">trousers</a></li>
+                <li><a href="#" class="footer-link">shoes</a></li>
+                <li><a href="#" class="footer-link">casuals</a></li>
+                <li><a href="#" class="footer-link">formals</a></li>
+                <li><a href="#" class="footer-link">sports</a></li>
+                <li><a href="#" class="footer-link">watches</a></li>
+            </ul>
+            <ul class="category">
+                <li class="category-title">women</li>
+                <li><a href="#" class="footer-link">t-shirts</a></li>
+                <li><a href="#" class="footer-link">sweatshirts</a></li>
+                <li><a href="#" class="footer-link">shirts</a></li>
+                <li><a href="#" class="footer-link">jeans</a></li>
+                <li><a href="#" class="footer-link">trousers</a></li>
+                <li><a href="#" class="footer-link">shoes</a></li>
+                <li><a href="#" class="footer-link">casuals</a></li>
+                <li><a href="#" class="footer-link">formals</a></li>
+                <li><a href="#" class="footer-link">sports</a></li>
+                <li><a href="#" class="footer-link">watches</a></li>
+            </ul>
+        </div>           
+    </div>
+    <p class="footer-title">about company</p>
+    <p class="info">mnbvcr mnbvfdrtyu mnbvftyu mnbgy 
+        oiuydcvbnjhgfrtyj mnbvftyu nbvcdertb mbgyj
+        bvfrtyhnmloiuygftyhn mkiug mnbgfd vcxsaq
+        hgftyhn bftyhnjm mnhyhjk kjhgfdrtyu 
+        nhytgb vfredc vbnjy nmkiuytr
+        ,mnbvcftghyjm vertghbn  cdertyhn vdrtyjm, cdrtyhj
+        ,bvcfhyj  vftyuj  vftyhjn vfrtyujnm  bvftyujm
+        ,mnbvfrtyujn bvfrtyhbn  mnbvcdrtyhjm nbvftyjk
+        kjbvcxdrth  vcfrtyhjnm  vcfrtyujkl vrtyujk,  vcfrtyh
+        kjbvcftyuj  vcdrtyhn mnbvcdrtyhjk????????/ frt6yujk
+        nbg mbgyjk jbvftyhj bgtyjk kjbvftyujk????</p>
+        <p class="info">support emails - help@clothing.com,
+        customersupport@clothing.com</p>
+        <p class="info">telephone - 180 00 00 0001, 100 180 00 002</p>
+        <p class="info">credits to - Vastra logo design : mr.debdut brahmachari</p>
+        <p class="info">credits to - UI design : mr.yougansh malik</p>
+        <div class="footer-social-container">
+            <div>
+                <a href="#" class="social-link">terms & services</a>
+                <a href="#" class="social-link">privacy page</a>
+            </div>
+            <div>
+                <a href="#" class="social-link">instagram</a>
+                <a href="#" class="social-link">facebook</a>
+                <a href="#" class="social-link">twitter</a>
+            </div>
+        </div>
+        <p class="footer-credit">Vastra, Best apparels online store</p>
+    
+    `;
+}
+
+createFooter();
